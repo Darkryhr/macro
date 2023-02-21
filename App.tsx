808eede22b0e72b7a0d8996ad3d1406371d0524b
Gabriel Lellouche <gabyk800@gmail.com>
2023-02-21 16:34:34 +0200

    style:custom fonts + safe view area

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import RootNavigator from '@navigation/RootNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Teko-Bold': require('./assets/fonts/Teko-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import React, { useEffect, useState } from 'react';
 import { Pressable, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen = ({ route, navigation }) => {
   const [totalDailyCalories, setTotalDailyCalories] = useState(0);
@@ -8,13 +9,25 @@ const HomeScreen = ({ route, navigation }) => {
   useEffect(() => {
     if (route.params?.calories) {
       setTotalDailyCalories(prev => prev + parseInt(route.params?.calories));
+      route.params.calories = 0;
     }
   }, [route.params?.calories]);
 
   return (
-    <View className='flex-1 items-center justify-center bg-gray-100'>
-      <Text className='text-8xl text-gray-700'>{totalDailyCalories}</Text>
-      <Text className='text-base'>Calories</Text>
+    <SafeAreaView className='flex-1 items-center justify-center bg-gray-100'>
+      <Text
+        className='text-gray-800 pt-8 text-center'
+        style={{
+          fontFamily: 'Teko-Bold',
+          fontSize: 100,
+          lineHeight: 80,
+          flexWrap: 'wrap',
+        }}
+      >
+        <Text className='text-blue-500'>{totalDailyCalories}</Text>
+        {'\n'}Calories
+      </Text>
+
       <View className='border-4 border-violet-500 rounded-full m-4'>
         <Pressable
           className='p-3'
@@ -23,7 +36,7 @@ const HomeScreen = ({ route, navigation }) => {
           <MaterialIcons name='add' size={40} color='#25292e' />
         </Pressable>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
